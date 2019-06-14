5522337dce59ba20bf691013113af40e421f96ab
t-mireal92 <mirielletush@gmail.com>
2019-06-14 15:20:52 +0300

    guessing game

---
+++ Number-Guessing-Game/main.js
@@ -0,0 +1,9 @@
+var num= (Math.floor(Math.random() * 100) +1);// generates random numbers//
+if (num%2==0)
+	console.log("number is even.")//diplays if num is even//
+else if (num%2==1)
+	console.log("number is odd.");//displays if num is odd//
+for (count = 1; count<=10; count++) //for the loop to count 10 times//
+{
+	guess= prompt("Guess num of your choice");//prompts user t input number//
+}
